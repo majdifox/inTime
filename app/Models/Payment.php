e760b75a9c513988281d3df407c9a4f18a95902b
majmehdi <majmehdi20@gmail.com>
2025-04-25 11:10:20 +0100

    adding the fillables to the payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,51 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Ride;
+use App\Models\User;
 
 class Payment extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ride_id',
+        'user_id',
+        'amount',
+        'payment_method',
+        'status',
+        'stripe_payment_id',
+        'payment_details',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'payment_details' => 'array',
+    ];
+
+    /**
+     * Get the ride associated with the payment.
+     */
+    public function ride()
+    {
+        return $this->belongsTo(Ride::class);
+    }
+
+    /**
+     * Get the user who made the payment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
