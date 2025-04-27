07b92cc3baafd25cb3ea84fa451c8361cdbb8d1c
majmehdi <majmehdi20@gmail.com>
2025-04-27 16:23:47 +0100

    deleteing the edit button in passenger private profile

--- resources/views/passenger/profile/private.blade.php
+++ resources/views/passenger/profile/private.blade.php
@@ -67,14 +67,7 @@
                             </div>
                         </div>
                         
-                        <div class="ml-auto">
-                        <a href="{{ route('passenger.profile.private') }}" class="inline-flex items-center px-4 py-2 border border-white rounded-md text-white hover:bg-white hover:text-gray-800 transition">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-    </svg>
-    Edit Profile
-</a>
-                        </div>
+            
                     </div>
                 </div>
                 
