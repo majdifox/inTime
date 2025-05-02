b9da36e2bc9f6d5e4fa9c73cdc901be2f3477873
majmehdi <majmehdi20@gmail.com>
2025-05-02 17:52:27 +0100

    the public profile is working as well

--- resources/views/driver/profile/private.blade.php
+++ resources/views/driver/profile/private.blade.php
@@ -194,13 +194,7 @@
                             </div>
                         @endif
                         
-                        <div class="pt-5">
-                            <div class="flex justify-end">
-                                <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                    Save Changes
-                                </button>
-                            </div>
-                        </div>
+                        
                     </form>
                 </div>
                 
